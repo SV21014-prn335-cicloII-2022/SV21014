330828601e7b5e66e1b9973d226f652fef38fe13
Kevin Francisco Sagastume Vinueza <sv21014@ues.edu.sv>
2022-09-03 00:00:18 -0600

    Creando test

---
+++ Guia02_2022/src/test/java/ues/occ/edu/sv/ingenieria/prn335/controller/TipoDocumentoBeanTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ues.occ.edu.sv.ingenieria.prn335.controller;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Sagastume
+ */
+public class TipoDocumentoBeanTest {
+    
+    public TipoDocumentoBeanTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
